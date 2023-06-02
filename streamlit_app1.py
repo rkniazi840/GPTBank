96efdf3a8bb41e17a66a57495ed7403bdd215354
Rizwan Khan Niazi <39627687+rkniazi840@users.noreply.github.com>
2023-06-02 22:07:59 +0500

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,4 +1,4 @@
- #Import os to set API key
+#Import os to set API key
 import os
 # Import OpenAI as main LLM service
 from langchain.llms import OpenAI
@@ -7,7 +7,7 @@ import streamlit as st
 
 # Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store 
+# Import chroma as the vector store
 from langchain.vectorstores import Chroma
 
 # Import vector store stuffpip install PyPDF2
@@ -20,14 +20,14 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = ' 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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 
 # Create and load PDF Loader
-loader =  PyPDFLoader(r"C:/GPTBank/file.pdf")
-# Split pages from pdf 
+loader =  PyPDFLoader(r"file.pdf")
+# Split pages from pdf
 pages = loader.load_and_split()
 # Load documents into vector database aka ChromaDB
 store = Chroma.from_documents(pages, collection_name='file')
@@ -58,9 +58,9 @@ if prompt:
     # ...and write it out to the screen
     st.write(response)
 
-    # With a streamlit expander  
+    # With a streamlit expander
     with st.expander('Document Similarity Search'):
         # Find the relevant pages
-        search = store.similarity_search_with_score(prompt) 
-        # Write out the first 
-        st.write(search[0][0].page_content) 
+        search = store.similarity_search_with_score(prompt)
+        # Write out the first
+        st.write(search[0][0].page_content)
